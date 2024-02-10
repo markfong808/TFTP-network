ccdd41552c10ccf38c50e3c86c9f62815baaba10
bopan-uw <91004152+bopan-uw@users.noreply.github.com>
2024-02-10 15:32:20 -0800

    Update TftpConstant.h

--- TftpConstant.h
+++ TftpConstant.h
@@ -2,5 +2,6 @@
  * TODO: Add constant variables in this file
  */
 static const unsigned int MAX_PACKET_LEN = 516; // data 512 + opcode 2 + block num 2
+static const int MAX_RETRY_COUNT = 10;
 static const char * SERVER_FOLDER = "server-files/"; // DO NOT CHANGE
 static const char * CLIENT_FOLDER = "client-files/"; // DO NOT CHANGE
